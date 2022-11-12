141c28488c63aed4dd01f3ff70d4b867a7df2cb3
shubham <shubhamrawatktw@gmail.com>
2022-11-12 16:20:24 +0530

    initial packages download and setup ready

---
+++ screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+const HomeScreen = () => {
+  return (
+   <View className="items-center justify-center">
+    <Text>
+        shubham
+    </Text>
+   </View>
+  )
+}
+
+export default HomeScreen
